f011bb3f2862b8f50b59faa6d3a42582bac8af7e
chichu <chichu@chichu-workstatinon.(none)>
2010-10-26 19:35:27 +0800

    add some code

--- scripts/restore_data.py
+++ scripts/restore_data.py
@@ -1,21 +1,37 @@
 #encoding:utf-8
 
 import os,sys
-from dbutil import DBUtils
+from dbutils import DBUtils
 
 DATA_ROOT = '/data1/dsipder/data/src/'
 
-for filename in os.path.listdir(DATA_ROOT):
-	path = os.path.join(DATA_ROOT,filename)
-	f = open(path,"rb")
-	while f.readline
+#db = DBUtils()
+
+for pathname in os.listdir(DATA_ROOT):
+    path = os.path.join(DATA_ROOT,pathname)
+    for filename in os.listdir(path):
+        data_file = os.path.join(path,filename)
+        f = open(data_file,"r")
+        column = {}
+        while 1:
+            line = f.readline()
+            if not line:
+                break
+            elif line == "@":
+                #db.insert("raw_data",column)
+                print column
+                column = {}
+                continue
+            repx = re.compile("^@(?P<name>.+):(?P<value>.+)")
+            if repx:
+                name = repx.groupdict()['name'].lower()
+                value = repx.groupdict()['value'].strip()
+                column[name] = value
+#db.commit()
+#db.close()
+                
 
 
-db = DBUtils()
-db.begin()
 
-db.insert("")
-
-db.commit()
 
 
